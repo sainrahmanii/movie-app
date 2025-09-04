f38bc20eefb0fae552bb4f3755cb74115452df84
sainrahmanii <husainrahmani127@gmail.com>
2025-09-04 14:10:00 +0700

    Refactor ProfileModel: reorder parameters and improve JSON serialization

--- lib/features/profile/data/models/profile_model.dart
+++ lib/features/profile/data/models/profile_model.dart
@@ -7,42 +7,30 @@ class ProfileModel extends Profile {
 
   const ProfileModel({
     required super.id,
-    required super.email,
     required this.firstName,
     required this.lastName,
+    required super.email,
     required this.avatar,
-  }) : super(fullName: "$firstName $lastName", avatarUrl: avatar);
+  }) : super(fullName: '$firstName $lastName', avatarUrl: avatar);
 
-  factory ProfileModel.fromJson(Map<String, dynamic> json) {
-    Map<String, dynamic> data = json['data'];
+  factory ProfileModel.fromJson(Map<String, dynamic> dataJson) {
+    Map<String, dynamic> data = dataJson["data"];
     return ProfileModel(
-      id: data['id'],
-      email: data['email'],
-      firstName: data['first_name'],
-      lastName: data['last_name'],
-      avatar: data['avatar'],
+      id: data["id"],
+      firstName: data["first_name"],
+      lastName: data["last_name"],
+      email: data["email"],
+      avatar: data["avatar"],
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
-      'id': id,
-      'email': email,
-      'first_name': firstName,
-      'last_name': lastName,
-      'avatar': avatar,
+      "id": id,
+      "email": email,
+      "firstName": firstName,
+      "lastName": lastName,
+      "avatar": avatar,
     };
-  }
-
-  static List<ProfileModel> fromJsonList(List jsonList) {
-    if (jsonList.isEmpty) return [];
-
-    return jsonList
-        .map((singleDataProfile) => ProfileModel.fromJson(singleDataProfile))
-        .toList();
-  }
-
-  static List<Map<String, dynamic>> toJsonList(List<ProfileModel> models) {
-    return models.map((model) => model.toJson()).toList();
   }
 }
